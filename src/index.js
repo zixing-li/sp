cf64ec12d5404586057f0845a50229b9a9c9975c
Zixing Li <zl82@cornell.edu>
2018-03-01 23:37:48 -0500

    add home and industry page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './containers/App';
+
+import Home from './components/Home';
+import Industry from './components/Industry';
+
+import { Provider } from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    {/* <Provider store={store}> */}
+      <App>
+        <Route exact path="/" component={Home} />
+        <Route path="/industry" component={Industry} />
+      </App>
+    {/* </Provider> */}
+  </BrowserRouter>
+), document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
