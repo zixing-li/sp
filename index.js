f5a24ce5850256168c867d45c553002e6ec1d9a9
Zixing Li <zl82@cornell.edu>
2018-04-01 14:05:18 -0400

    index debug

--- index.js
+++ index.js
@@ -38,20 +38,5 @@ if (process.env.NODE_ENV === 'production' ) {
   });
 }
 
-// if (process.env.NODE_ENV === 'production' ) {
-//   const path = require('path');
-
-//   // Express will serve up production assets like our main.js file, or main.css file
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-
-//   // Express will serve up the index.html file if it doesn't recognize the route (if not in authRoutes, not in billingRoutes, not in build directory, then move on to ReactRouter)
-  
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
 const PORT = process.env.PORT || 5000; // env = environment variables. If no environment variable (Eg. local machine), use default - in this case 5000
 app.listen(PORT);
