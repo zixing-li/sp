1f82e30c3bbed7d4f1097cdc30a32f03ef6e2d50
Zixing Li <zl82@cornell.edu>
2018-04-01 13:53:25 -0400

    index debug

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production' ) {
 //   });
 // }
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 const PORT = process.env.PORT || 5000; // env = environment variables. If no environment variable (Eg. local machine), use default - in this case 5000
 app.listen(PORT);
