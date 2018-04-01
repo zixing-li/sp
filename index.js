ead2b384ac7475329c1ef18f4ed438bd47ffabd0
Zixing Li <zl82@cornell.edu>
2018-03-31 23:39:42 -0400

    debug index.js

--- index.js
+++ index.js
@@ -27,16 +27,31 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); // turns into a function and call the express app object
 require('./routes/billingRoutes')(app);
 
+// if (process.env.NODE_ENV === 'production' ) {
+//   // Express will serve up production assets like our main.js file, or main.css file
+//   app.use(express.static('client/build'));
+
+//   // Express will serve up the index.html file if it doesn't recognize the route (if not in authRoutes, not in billingRoutes, not in build directory, then move on to ReactRouter)
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
 if (process.env.NODE_ENV === 'production' ) {
+  const path = require('path');
+
   // Express will serve up production assets like our main.js file, or main.css file
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Express will serve up the index.html file if it doesn't recognize the route (if not in authRoutes, not in billingRoutes, not in build directory, then move on to ReactRouter)
-  const path = require('path');
+  
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const PORT = process.env.PORT || 5000; // env = environment variables. If no environment variable (Eg. local machine), use default - in this case 5000
 app.listen(PORT);
