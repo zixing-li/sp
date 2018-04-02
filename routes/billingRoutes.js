ee6252cf538ab10c2ad5dcaebc1b413db3a43340
Zixing Li <zl82@cornell.edu>
2018-04-01 21:25:46 -0400

    add email template, fix typo

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,17 +2,18 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
-module.exports = (app) => {
+module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
-      amount: 500, // need to say again on the backend
+      amount: 500,
       currency: 'usd',
-      description: '$5 payment',
-      source: req.body.id // tok_......
+      description: '$5 for 5 credits',
+      source: req.body.id
     });
 
     req.user.credits += 5;
-    const user = await req.user.save(); // save is an async request
+    const user = await req.user.save();
+
     res.send(user);
   });
 };
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys'); // get access to mongoose model class. Written this way to avoid erros in tests, therefore not use require('surveys')
 
 module.exports = (app) => {
-  app.post('/api/surveys/post', requireLogin, requireCredits, (req, res) => { // we can add in any number of functions in the request handler
+  app.post('/api/surveys/post', requireLogin, requireCredits, async (req, res) => { // we can add in any number of functions in the request handler
     const { title, subject, body, recipients } = req.body; // pull (get access to) these properties that were passed along from express from req.body
     
     const survey = new Survey({ // create a survey instance
@@ -22,5 +22,17 @@ module.exports = (app) => {
 
     // send an email
     const mailer = new Mailer(survey, surveyTemplate(survey));
+    
+    try {
+      await mailer.send();
+      // wait for the above is finished
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
