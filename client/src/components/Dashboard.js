ca6839c5b7afa76b9af0450af145d68ae4b198ba
Zixing Li <zl82@cornell.edu>
2018-04-08 17:58:30 -0400

    add dashboard display of surveys

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
   return (
     <div>
-      <Link to="/surveys/new" type="button" className="btn btn-primary">Add Survey</Link>
+      <SurveyList />
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn btn-primary">
+          Add Survey
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 import industriesReducer from './industriesReducer';
 import packagesReducer from './packagesReducer';
 import { reducer as reduxForm } from 'redux-form'; // rename reducer to reduxForm to avoid confusion
@@ -8,7 +9,8 @@ const rootReducer = combineReducers({
   industries: industriesReducer,
   packages: packagesReducer,
   auth: authReducer,
-  form: reduxForm // provided by redux-form
+  form: reduxForm, // provided by redux-form
+  surveys: surveysReducer,
 });
 
 export default rootReducer;
