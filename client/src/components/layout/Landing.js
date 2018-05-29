c0a9f4423f48a112712d1cfe6f4436c8fa981ef0
Zixing Li <zl82@cornell.edu>
2018-05-28 22:38:37 -0400

    rename

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -50,12 +50,8 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
-                <p className="lead">
-                  {" "}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
-                </p>
+                <h1 className="display-3 mb-4">StarterPack</h1>
+                <p className="lead"> Get advise from people who's been there</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -64,7 +64,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevConnector
+            StarterPack
           </Link>
           <button
             className="navbar-toggler"
@@ -79,7 +79,7 @@ class Navbar extends Component {
               <li className="nav-item">
                 <Link className="nav-link" to="/profiles">
                   {" "}
-                  Developers
+                  Contributors
                 </Link>
               </li>
             </ul>
